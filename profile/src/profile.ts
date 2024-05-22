7b31d26ee4eb4b48c1b55d68149e06a02bcd8327
chano64123 <promo201674@gmail.com>
2024-05-22 12:25:44 -0500

    operadores avanzados

--- profile/src/profile.ts
+++ profile/src/profile.ts
@@ -33,3 +33,34 @@ const familiar2: IProfile = {
 const family: IProfile[] = [data, familiar1, familiar2]
 
 console.log(family);
+
+console.log("Operador TypeOf");
+console.log(`family es del tipo: ${typeof family}`);
+console.log(`data es del tipo: ${typeof data}`);
+console.log(`familiar1 es del tipo: ${typeof familiar1}`);
+console.log(`nombre es del tipo: ${typeof nombre}`);
+console.log(`edad es del tipo: ${typeof edad}`);
+console.log(`hobbies es del tipo: ${typeof hobbies}`);
+
+console.log("\nOperador In");
+const propiedades = ['name', 'nombre', 'age', 'profesion', 'peso', 'altura', 'hobby', 'hobbies']
+for (const propiedad of propiedades) {
+  const esta = propiedad in data ? 'está' : 'no está';
+  console.log(`La propiedad "${propiedad}" ${esta} en el objeto data.`);
+}
+
+console.log("\nOperador Is");
+const verificar = [
+  ['data', data, 'object'],
+  ['data', data, 'number'],
+  ['edad', edad, 'number'],
+  ['edad', edad, 'string'],
+  ['nombre', nombre, 'number'],
+  ['nombre', nombre, 'string'],
+  ['familiar1', familiar1, 'boolean']
+]
+for (const [nombre, propiedad, tipo] of verificar) {
+  const es = typeof propiedad === tipo ? 'es' : 'no es';
+  console.log(`La propiedad "${nombre}" ${es} del tipo ${tipo}.`);
+}
+
